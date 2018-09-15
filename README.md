497792da80713cba44bf26692fee561c6db50548
TonnyL <lizhaotailang@gmail.com>
2018-09-16 02:22:35 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Moka
+
+An Android app for github.com with Material Theme 2.
+
+# LICENSE
+Carol is under an MIT license. See the [LICENSE](LICENSE) file for more information.
---
+++ app/src/main/java/io/github/tonnyl/moka/MokaApp.kt
@@ -0,0 +1,12 @@
+package io.github.tonnyl.moka
+
+import android.app.Application
+
+class MokaApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+}

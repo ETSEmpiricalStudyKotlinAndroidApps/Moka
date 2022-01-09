e84bb4fc6a45b84f5936c5cc37782efcc18eb19c
TonnyL <lizhaotailang@gmail.com>
2022-01-09 17:32:32 +0800

    Fix json serializers modules

--- common/src/commonMain/kotlin/io/tonnyl/moka/common/serialization/Json.kt
+++ common/src/commonMain/kotlin/io/tonnyl/moka/common/serialization/Json.kt
@@ -3,13 +3,14 @@ package io.tonnyl.moka.common.serialization
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.contextual
+import kotlinx.serialization.modules.plus
 
 val json by lazy {
     Json {
         encodeDefaults = true
         isLenient = true
         ignoreUnknownKeys = true
-        serializersModule = SerializersModule {
+        serializersModule += SerializersModule {
             contextual(InstantSerializer)
         }
     }
